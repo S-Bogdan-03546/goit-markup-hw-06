01ddae167f59cc66d4634ba8b7871ba089a08f8c
Bogdan S <s.bogdan.03546@gmail.com>
2024-12-05 16:57:12 +0200

    commit pinned hover

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,20 @@
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector('[data-menu-open]'),
+
     // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-menu-close]'),
+
+    // Додати ідентифікатор id="mob-nav-menu" насписок посилань (для закриття вікна меню при переході за посиланнями)
+    closeModallinks: document.querySelector('#mob-nav-menu'),
+
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModallinks.addEventListener('click', toggleModal);
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
